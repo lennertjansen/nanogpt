d4925e560ba4c36e1fc678c07f7508fa31af639e
Lennert Jansen <lennertjansen95@gmail.com>
2026-08-19 11:22:48 +0200

    fix: create generation context on device

--- bigram.py
+++ bigram.py
@@ -147,5 +147,5 @@ for iter in range(max_iters):
     
 
 # generate from the model
-context = idx=torch.zeros((1, 1), dtype=torch.long)
+context = idx=torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
